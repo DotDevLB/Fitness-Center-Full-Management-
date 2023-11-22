e388008ab11858db6a609cc25284b841cc310de1
Joe Hadchity <hadchityjoe64@gmail.com>
2023-11-22 15:28:52 +0200

    Springboot-React.js Connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/');
+      console.log('Response:', response); // Log the response
+      const data = response.data; // Access the response data directly
+      console.log('Data:', data); // Log the data received
+      setMessage(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Response from Spring Boot:</h1>
+      <p>{message}</p>
     </div>
   );
 }
